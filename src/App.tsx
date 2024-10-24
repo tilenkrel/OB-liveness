9d954610d76c4a55ffa374a3e989d70ea53e502d
tilenkrel <tilen.krel@gmail.com>
2024-10-24 20:25:25 +0200

    removed unused variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+//import React from 'react';
 import { Loader, ThemeProvider } from '@aws-amplify/ui-react';
-import { Amplify } from 'aws-amplify';
+//import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
 //import awsexports from './aws-exports';
 
